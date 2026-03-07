74017901aab64a1e675eaeee8661adfc15b371dc
Kaung San Phyoe <davidkaung@gmail.com>
2026-03-07 13:54:16 +0630

    Remove agent builder feature and dependencies

--- app/builder/page.tsx
+++
@@ -1,9 +0,0 @@
-import { BuilderCanvas } from "@/components/builder/BuilderCanvas";
-
-export default function BuilderPage() {
-  return (
-    <main className="h-screen w-screen overflow-hidden">
-      <BuilderCanvas />
-    </main>
-  );
-}
